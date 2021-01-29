35a71afb521bc57d0906566f19fd344f1062d1bc
atlasfoo@lin <jf_mejiar547@live.com>
2021-01-28 23:47:16 -0600

    logo

--- components/Header/TopBar/TopBar.jsx
+++ components/Header/TopBar/TopBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
+import Link from 'next/link';
 
 export default function TopBar() {
   return (
-    <div>
-      TopBar
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+          <Grid.Column width={8} className="top-bar__left">
+            <Logo/>
+          </Grid.Column>
+          <Grid.Column width={8} className="top-bar__right">
+            <h2>Buscador</h2>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
+  );
+}
+
+
+function Logo(){
+  return (
+    <Link href='/'>
+      <Image src='/logo.png' alt='Gaming' />
+    </Link>
   )
 }
