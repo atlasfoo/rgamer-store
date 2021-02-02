77b3bf75a112c199f4bd22a9bd9e155b21f78c46
atlasfoo@lin <jf_mejiar547@live.com>
2021-02-01 23:53:35 -0600

    basic modal

--- client/components/Header/Menu/NavMenu.jsx
+++ client/components/Header/Menu/NavMenu.jsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Container, Grid, Icon, Menu } from "semantic-ui-react";
+import BasicModal from "../../Modal/BasicModal";
 
 export default function NavMenu() {
+  const [showModal, setShowModal] = useState(false);
+
+  const onShowModal = () => setShowModal(true);
+
   return (
     <nav className="menu">
       <Container>
@@ -11,10 +16,18 @@ export default function NavMenu() {
             <MenuPlatforms />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            <MenuOptions />
+            <MenuOptions onShowModal={onShowModal} />
           </Grid.Column>
         </Grid>
       </Container>
+      <BasicModal
+        show={showModal}
+        setShow={setShowModal}
+        title="Inicia Sesi&oacute;n"
+        size="small"
+      >
+        <h2>Modal</h2>
+      </BasicModal>
     </nav>
   );
 }
@@ -34,10 +47,11 @@ function MenuPlatforms() {
     </Menu>
   );
 }
-function MenuOptions() {
+
+function MenuOptions({ onShowModal }) {
   return (
     <Menu>
-      <Menu.Item>
+      <Menu.Item onClick={onShowModal}>
         <Icon name="user outline" /> Mi cuenta
       </Menu.Item>
     </Menu>
