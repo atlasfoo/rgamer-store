639da1168cff5879f1d03514835ea10430a5713a
atlasfoo@lin <jf_mejiar547@live.com>
2021-02-14 00:47:03 -0600

    forgot password option

--- client/api/user.js
+++ client/api/user.js
@@ -43,7 +43,29 @@ const login = async (data) => {
     const result = await response.json();
 
     return result;
-  } catch (error) {}
+  } catch (error) {return null;}
 };
 
-export { create, login };
+const resetPassword = async (email) => {
+  try {
+    const url = `${BASE_PATH}/auth/forgot-password`;
+
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email}),
+    };
+
+    const response = await fetch(url, params);
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    return null;
+  }
+}
+
+export { create, login, resetPassword };
--- client/components/Auth/Forms/LoginForm/LoginForm.jsx
+++ client/components/Auth/Forms/LoginForm/LoginForm.jsx
@@ -2,18 +2,20 @@ import { useFormik } from "formik";
 import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 import { initalValues, validationSchema } from "./LoginFormValues";
+import * as Yup from 'yup';
 
 import * as UserApi from "../../../../api/user";
 import { toast } from "react-toastify";
 import useAuth from "../../../../hooks/useAuth";
 
 export default function LoginForm({ showRegisterForm, onCloseModal }) {
+  
   const onSubmit = async (data) => {
     setLoading(true);
     const response = await UserApi.login(data);
     console.log(response);
     if (response?.jwt) {
-      login(response?.jwt)
+      login(response?.jwt);
       onCloseModal();
     } else {
       toast.error("Usuario o contraseña incorrecta");
@@ -21,13 +23,22 @@ export default function LoginForm({ showRegisterForm, onCloseModal }) {
     setLoading(false);
   };
 
-  const {session, login} = useAuth();
+  const resetPassword = () => {
+    setErrors({});
+    const validateEmail = Yup.string().email(true).required(true);
+    if(!validateEmail.isValidSync(values.identifier)){
+      toast.error("Email inválido");
+      setErrors({identifier: true});
+    } else {
+      UserApi.resetPassword(values.identifier);
+    }
+  }
 
-  console.log(session);
+  const { login } = useAuth();
 
   const [loading, setLoading] = useState(false);
 
-  const { handleChange, handleSubmit, errors } = useFormik({
+  const { handleChange, handleSubmit, errors, setErrors, values } = useFormik({
     initialValues: initalValues,
     validationSchema: validationSchema,
     onSubmit: onSubmit,
@@ -58,7 +69,7 @@ export default function LoginForm({ showRegisterForm, onCloseModal }) {
           <Button className="submit" type="submit" loading={loading}>
             Entrar
           </Button>
-          <Button type="button">¿Has olvidado la contraseña?</Button>
+          <Button type="button" onClick={resetPassword}>¿Has olvidado la contraseña?</Button>
         </div>
       </div>
     </Form>
