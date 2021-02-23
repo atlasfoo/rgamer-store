bfaa7adaff7363c153ff907630361e69ac78412c
atlasfoo@win <jf_mejiar547@live.com>
2021-02-23 00:13:56 -0600

    complete rendered list

--- client/components/GamesList/GamesList.jsx
+++ client/components/GamesList/GamesList.jsx
@@ -1,12 +1,41 @@
-import { map } from 'lodash'
-import React from 'react'
+import { map } from "lodash";
+import Link from "next/link";
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 
-const GamesList = ({games}) => {
+const GamesList = ({ games }) => {
   return (
-    <div className='list-games'>
-      {map(games, (game) => (<h3>{game.title}</h3>) )}
+    <div className="list-games">
+      <Grid>
+        <Grid.Row columns={5}>
+          {map(games, (game) => (
+            <Game key={game.id} game={game}/>
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
-  )
+  );
+};
+
+function Game({ game }) {
+  return (
+    <Grid.Column className="list-games__game">
+      <Link href={`/${game.url}`}>
+        <a>
+          <div className="list-games__game-poster">
+            <Image src={game.poster.url} alt={game.title}/>
+            <div className="list-games__game-poster-info">
+              {game.discount ? (
+                <span className='discount'>-{game.discount}%</span>
+              ) : ''}
+              <span className='price'>{game.price}$</span>
+            </div>
+          </div>
+          <h2>{game.title}</h2>
+        </a>
+      </Link>
+    </Grid.Column>
+  );
 }
 
-export default GamesList
+export default GamesList;
