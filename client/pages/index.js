f3bbca1f2d3b46964cdf4a1e19e59d18da304896
atlasfoo@win <jf_mejiar547@live.com>
2021-02-22 23:42:42 -0600

    game list init

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import BasicLayout from "../layouts/BasicLayout";
+import * as gameApi from '../api/game'
+import { size } from "lodash";
 
 export default function Home() {
+  const [games, setGames] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const response = await gameApi.getLastGames(20);
+      if(size(response) > 0) setGames(response);
+      setGames([])
+    })()
+  }, [input])
   return (
     <BasicLayout>
       <h1>Hola desde el children</h1>
