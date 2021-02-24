946b049eca80e148f8cb7122050b6d81da3f8a8b
atlasfoo@win <jf_mejiar547@live.com>
2021-02-23 21:55:32 -0600

    pages per platform

--- client/api/game.js
+++ client/api/game.js
@@ -11,3 +11,25 @@ export const getLastGames = async (limit) => {
     return null;
   }
 };
+
+export const getGamesByPlatform = async (platform, limit, start) => {
+  try {
+    const url = `${BASE_PATH}/games?platform.url=${platform}&_limit=${limit}&_sort=createdAt:desc&_start=${start}`;
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }  
+}
+
+export const getTotalGamesPlatform = async (platform) => {
+  try {
+    const url = `${BASE_PATH}/games/count?platform.url=${platform}&_limit=${limit}&_sort=createdAt:desc&_start=${start}`;
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }  
+}
--- client/pages/games/[platform].js
+++ client/pages/games/[platform].js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BasicLayout from "../../layouts/BasicLayout/BasicLayout";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import * as gameApi from "../../api/game";
+import { Loader } from "semantic-ui-react";
+import { parseInt, size } from "lodash";
+import GamesList from "../../components/GamesList/GamesList";
+
+const limitPerPage = 5;
 
 const platform = () => {
-  const {query} = useRouter();
+  const { query } = useRouter();
+
+  const [games, setGames] = useState(null);
+  const [totalGames, setTotalGames] = useState(null);
+
+  const getStartItem = () => {
+    const currentPages = parseInt(query.page);
+    if(query.page || currentPages === 1 ) return 0;
+    else return currentPages * limitPerPage - limitPerPage;
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (query.platform) {
+        const response = await gameApi.getGamesByPlatform(
+          query.platform,
+          limitPerPage,
+          0
+        );
+        setGames(response);
+      }
+    })();
+
+    return () => setGames(null);
+  }, [query]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await gameApi.getTotalGamesPlatform(query.platform);
+      setTotalGames(response);
+    })()
+  }, [query])
 
   return (
     <BasicLayout className="platform">
-      <h1>Estamos en plataformas: {query.platform}</h1>
+      {!games && <Loader active>Cargando Juegos</Loader>}
+
+      {games && size(games) === 0 && (
+        <div>
+          <h3>No hay juegos dispoibles</h3>
+        </div>
+      )}
+
+      {size(games) > 0 && <GamesList games={games} />}
     </BasicLayout>
   );
 };
