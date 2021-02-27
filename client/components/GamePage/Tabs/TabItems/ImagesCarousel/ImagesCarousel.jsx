b70cbb4fb14295887f93d45cebb704391e80bd38
atlasfoo@win <jf_mejiar547@live.com>
2021-02-26 20:34:37 -0600

    image modal game info

--- client/components/GamePage/Tabs/TabItems/ImagesCarousel/ImagesCarousel.jsx
+++ client/components/GamePage/Tabs/TabItems/ImagesCarousel/ImagesCarousel.jsx
@@ -1,35 +1,53 @@
-import { map, size } from 'lodash'
-import React from 'react'
-import Slider from 'react-slick'
-import { Image } from 'semantic-ui-react'
+import { map, size } from "lodash";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import { Image, Modal } from "semantic-ui-react";
 
 const settings = {
-  className: 'carousel-screenshots',
+  className: "carousel-screenshots",
   dots: false,
   infinite: true,
   speed: 500,
   slidesToShow: 1,
-  swipeToSlide: true
-}
+  swipeToSlide: true,
+};
 
-const ImagesCarousel = ({title, images}) => {
+const ImagesCarousel = ({ title, images }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const openImage = (url) => {
+    setImageUrl(url);
+    setShowModal(true);
+  };
+
   return (
     <>
-    {(images && size(images) > 0)? (
-      <Slider {...settings}>
-        {map(images, (image) => (
-          <Image 
-            key={image.id}
-            src={image.url}
-            alt={image.name}
-            onClick={()=>console.log("abrir imagen")}
-          />
-        ))}
-      </Slider>
-    )
-       : <h5>No hay capturas disponibles</h5> }
+      {images && size(images) > 0 ? (
+        <>
+          <Slider {...settings}>
+            {map(images, (image) => (
+              <Image
+                key={image.id}
+                src={image.url}
+                alt={image.name}
+                onClick={() => openImage(image.url)}
+              />
+            ))}
+          </Slider>
+          <Modal
+            open={showModal}
+            onClose={() => setShowModal(false)}
+            size='large'
+          >
+            <Image src={imageUrl} alt={title} />
+          </Modal>
+        </>
+      ) : (
+        <h5>No hay capturas disponibles</h5>
+      )}
     </>
   );
-}
+};
 
-export default ImagesCarousel
+export default ImagesCarousel;
