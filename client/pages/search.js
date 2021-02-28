75fa7ba574859f90a66f8e0bc74fe20a2aea4394
atlasfoo@win <jf_mejiar547@live.com>
2021-02-27 23:31:34 -0600

    made search page

---
+++ client/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicLayout from '../layouts/BasicLayout/BasicLayout'
+
+const search = () => {
+  return (
+    <BasicLayout className="search">
+      <h3>Busqueda</h3>
+    </BasicLayout>
+  )
+}
+
+export default search
