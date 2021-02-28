2dcf07b353775b65500710733c01ed9958689ebc
atlasfoo@win <jf_mejiar547@live.com>
2021-02-27 23:42:14 -0600

    search field hooked

--- client/pages/search.js
+++ client/pages/search.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import BasicLayout from '../layouts/BasicLayout/BasicLayout'
 
 const search = () => {
+
+  useEffect(() => {
+    document.getElementById("search-game").focus();
+  }, [])
+
   return (
     <BasicLayout className="search">
       <h3>Busqueda</h3>
