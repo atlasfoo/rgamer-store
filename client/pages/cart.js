ce835612508a232c8f943895196ebfa641da3efe
atlasfoo@win <jf_mejiar547@live.com>
2021-03-03 00:04:04 -0600

    cart page

---
+++ client/pages/cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicLayout from '../layouts/BasicLayout/BasicLayout'
+
+const cart = () => {
+  return (
+    <BasicLayout className="cart">
+      <h1>Cart</h1>
+    </BasicLayout>
+  )
+}
+
+export default cart
