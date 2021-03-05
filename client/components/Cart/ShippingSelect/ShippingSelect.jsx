fc34feedf767fbe41e8703deabc2087c00944cfe
atlasfoo@win <jf_mejiar547@live.com>
2021-03-04 23:57:25 -0600

    address list display

--- client/components/Cart/ShippingSelect/ShippingSelect.jsx
+++ client/components/Cart/ShippingSelect/ShippingSelect.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import * as addressApi from "../../../api/address";
 import React, { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
+import { map, size } from "lodash";
+import { Grid } from "semantic-ui-react";
 
 const ShippingSelect = () => {
   const [addresses, setAddresses] = useState(null);
@@ -13,7 +15,44 @@ const ShippingSelect = () => {
       setAddresses(response || []);
     })();
   }, []);
-  return <div>Direccion de envio</div>;
+
+  console.log(addresses);
+
+  return (
+    <div className="shipping-select">
+      <div className="title">Direcci&oacute;n de env&iacute;o</div>
+      <div className="data">
+        {size(addresses) === 0 ? (
+          <h3>
+            No hay ninguna direccion creada
+            <Link href="/account">
+              <a>Añadir tu primera direcci&oacute;n</a>
+            </Link>
+          </h3>
+        ) : (
+          <Grid>
+            {map(addresses, (address) => (
+              <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+                <AddressCard address={address}/>
+              </Grid.Column>
+            ))}
+          </Grid>
+        )}
+      </div>
+    </div>
+  );
 };
+
+function AddressCard({address}){
+  return(
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>{address.city}, {address.state}, {address.postalCode}</p>
+      <p>{address.phone}</p>
+    </div>
+  )
+}
 
 export default ShippingSelect;
