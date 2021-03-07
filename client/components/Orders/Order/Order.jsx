bee978abf9789efef301e763aed427907dafa1b3
atlasfoo@win <jf_mejiar547@live.com>
2021-03-07 00:53:42 -0600

    order detail info

--- client/components/Orders/Order/Order.jsx
+++ client/components/Orders/Order/Order.jsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Icon, Image } from "semantic-ui-react";
 import moment from "moment";
+import BasicModal from "../../Modal/BasicModal";
 
 const Order = ({ order }) => {
   const { game, totalPayment, createdAt, shippingAddress } = order;
   const { title, poster, url } = game;
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
@@ -28,11 +31,31 @@ const Order = ({ order }) => {
           <p className="order__other-date">
             {moment(createdAt).format("L")} - {moment(createdAt).format("LT")}
           </p>
-          <Icon name="eye" circular link onClick={() => console.log("A")} />
+          <Icon name="eye" circular link onClick={() => setShowModal(true)} />
         </div>
       </div>
+      <AddressModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title={title}
+        shippingAddress={shippingAddress}
+      />
     </>
   );
 };
+
+function AddressModal({ showModal, setShowModal, title, shippingAddress }) {
+  return (
+    <BasicModal show={showModal} setShow={setShowModal} size="tiny" title={title}>
+      <h3>El pedido se ha enviado a la siguiente direcci&oacute;n</h3>
+      <div>
+        <p>{shippingAddress.name}</p>
+        <p>{shippingAddress.address}</p>
+        <p>{shippingAddress.state}, {shippingAddress.city}, {shippingAddress.postalCode}</p>
+        <p>{shippingAddress.phone}</p>
+      </div>
+    </BasicModal>
+  );
+}
 
 export default Order;
